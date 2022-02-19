820d833a615be75953beb29e2f7beab1fc2dee55
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-02-19 16:32:08 -0600

    update script
    count number of contigs > 1000 ; count number of bp in assembly
    save contigs > 1000 to contigsonethousand.txt
    append contig and bp statements to miniproject.log

--- miniproject.py
+++ miniproject.py
@@ -1,6 +1,30 @@
 import os
+from Bio import SeqIO
 #os.chdir(os.path.dirname(os.path.abspath('~/Downloads/sratoolkit.3.0.0-mac64/bin/')))
 #print(os.getcwd())
+
+#''' Got this part to work!
+
+#file = '/Users/sidra/Downloads/sratoolkit.3.0.0-mac64/bin/SRRnum'
+#infile = open(file, 'r').read()
+#directory = '/Users/sidra/Downloads/sratoolkit.3.0.0-mac64/bin/'
+#def prefetchcode(SRA,directory):
+   
+#    prefetchcommand = './prefetch ' + SRA + ' -O ' + directory
+    
+    #prefetchcommand = './prefetch --option-file ' + SRA
+    
+#    print(prefetchcommand)
+#    os.system(prefetchcommand)
+    
+    #sracommand = './fastq-dump -I --split-files *.sra'
+    #os.system(sracommand)
+
+#for i in infile:
+#    prefetchcode(i,directory)
+    
+#    '''
+
 
 ''' Got this part to work!
 #directory = '/Users/sidra/Downloads/sratoolkit.3.0.0-mac64/bin/'
@@ -31,7 +55,6 @@ def fastqdcode(sra):
     
 fastqdcode('SRR8185310.sra')
 '''
-
 #def sra_uncompress(*sra):
 #    sracommand = './fastq-dump -I --split-files *.sra'
 #    os.system(sracommand)
@@ -42,6 +65,7 @@ fastqdcode('SRR8185310.sra')
 #    sracommand = './fastq-dump -I --split-files *.sra'
 #    os.system(sracommand)
 
+# change to where you want results folder to be located
 outputpath = '/Users/sidra/Downloads/COMP_383-483_compbio/COMPBIO_MiniProject'
 def run_spades(outputpath,rtype='se',*readfiles):
     spadescommand = 'spades.py'
@@ -54,10 +78,35 @@ def run_spades(outputpath,rtype='se',*readfiles):
             spadescommand += ' -k 33,55,77,99,127 -t 2' + ' --only-assembler -s ' + readfiles[0] + ' -o ' + outputpath + '/results'
             print(spadescommand)
             with open('miniproject.log','a') as output: 
-                output.write(spadescommand)
+                output.write(spadescommand + '\n')
                 output.close()
             os.system(spadescommand)
             #print(spadescommand)
+    #os.system(spadescommand)
     return(os.path.exists(outputpath+'/results'))
             
 run_spades(outputpath,'se','SRR8185310_1.fastq')
+
+f = outputpath+'/results/contigs.fasta'
+sequence = []
+for record in SeqIO.parse(f, "fasta"):
+    sequence.append(str(record.seq))
+count = 0
+output = open('contigsonethousand.txt','w')
+for i in sequence:
+    if len(i) > 1000:
+        count += 1
+        output.write(i + '\n')
+output.close()
+
+f = '/Users/sidra/Downloads/COMP_383-483_compbio/COMPBIO_MiniProject/contigsonethousand.txt'
+infile = open(f,'r').read().split()
+num = len(infile)
+with open('miniproject.log','a') as output:
+    output.write('There are ' + str(num) + ' contigs > 1000 in the assembly.' + '\n')
+
+count = 0
+for i in infile:
+    count += len(i)
+with open('miniproject.log','a') as output:
+    output.write('There are ' + str(count) + ' bp in the assembly.' + '\n')
